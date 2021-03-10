4b3dc0f43e6e375f4b4f5fb2481181d78f703d82
Muhammed Ali <muhamzyali@gmail.com>
2021-03-10 17:07:09 +0100

    updated ./tests/test_infogami/__init__.py

--- tests/test_infogami/__init__.py
+++ tests/test_infogami/__init__.py
@@ -32,8 +32,8 @@ def teardown_module(module):
 def monkey_patch_browser():
     def check_errors(self):
         errors = [self.get_text(e) for e in
-                  self.get_soup().findAll(attrs={'id': 'error'}) +
-                  self.get_soup().findAll(attrs={'class': 'wrong'})]
+                  self.get_soup().findAll(attrs={'id': 'error'}) 
+                  + self.get_soup().findAll(attrs={'class': 'wrong'})]
         if errors:
             raise web.BrowserError(errors[0])
 
