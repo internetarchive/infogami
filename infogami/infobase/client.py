532400027aa3882adf3bde12a8dc59d52b278238
Christian Clauss <cclauss@me.com>
2021-04-09 16:27:55 +0200

    Define repr(Nothing()) and str(Nothing())

--- infogami/infobase/client.py
+++ infogami/infobase/client.py
@@ -640,12 +640,14 @@ class Nothing:
     """For representing missing values.
 
     >>> n = Nothing()
+    >>> repr(n)
+    '<Nothing>'
     >>> str(n)
-    '<Nothing>'
+    ''
     >>> web.safestr(n)
-    '<Nothing>'
+    ''
     >>> str([n])  # See #148 and #151
-    '[<Nothing>]'
+    '[]'
     """
     def __getattr__(self, name):
         if name.startswith('__') or name == 'next':
@@ -685,6 +687,9 @@ class Nothing:
 
     def __repr__(self):
         return "<Nothing>"
+
+    def __str__(self):
+        return ""
 
 nothing = Nothing()
 
