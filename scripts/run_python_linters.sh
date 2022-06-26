83e6dd9abf7d8bd5b23752378f332607a030de37
Christian Clauss <cclauss@me.com>
2022-06-26 08:55:58 +0200

    PEP8: Do not use bare except:

--- scripts/run_python_linters.sh
+++ scripts/run_python_linters.sh
@@ -5,7 +5,7 @@ set -e -v
 # Run linters and formatters
 black --skip-string-normalization --check .
 codespell --ignore-words-list=asend,alo,ba,spawnve
-flake8 . --count --ignore=E203,E402,E722,E731,F811,F841,W503 \
+flake8 . --count --ignore=E203,E402,E731,F811,F841,W503 \
          --max-complexity=43 --max-line-length=175 --show-source --statistics
 # exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
